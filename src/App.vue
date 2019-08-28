61c387c790ffef3b0bad1a06b8ff3b06c7af5ec1
SherilyShieh <1252524051@qq.com>
2019-08-28 09:41:37 +0800

    feat: day5

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,16 @@
       v-model="selectLabel"
       :data="tabs"
       @change="changeHandler">
+      <cube-tab v-for="(item, index) in tabs"
+        :icon="item.icon" :label="item.value" :key="index">
+        <div> {{item.label}} </div>
+        <span class="badge" v-if="item.label == 'Cart'"> {{cartTotal}} </span>
+      </cube-tab>
     </cube-tab-bar>
   </div>
 </template>
 <script>
+import { mapGetters } from 'vuex';
 export default {
   data() {
     return{
@@ -34,6 +40,9 @@ export default {
       ]
     }
   },
+  computed: {
+    ...mapGetters(['cartTotal'])
+  },
   created( ){
     //初始化页签设置，避免页面刷新
     this.selectLabel = this.$route.path;
@@ -54,7 +63,7 @@ export default {
   }
 }
 </script>
-<style>
+<style lang="stylus" scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -62,5 +71,36 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.cube-tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background: #edf0f4;
+}
+.cube-tabe-bar-slider {
+  top: 0;
+}
+// 动画
+.route-move-enter { // 入场状态
+  transform: translate3d(-100%, 0, 0);
+}
+.route-move-leave-to {// 离场状态
+  transform: translate3d(100%, 0, 0);
+}
+.route-move-enter-active, .route-move-leave-active { // 激活状态
+  transition: transform 0.3s;
+}
+.child-view { // 添加到每个页面上的样式，确保页面间补挤占位置
+  position:absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  padding-bottom: 40px;
+}
+.cube-tab {
+
 }
 </style>
---
+++ src/components/CartAnim.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="ball-wrap">
+        <transition
+            @before-enter="beforeEnter"
+            @enter="enter"
+            @afterEnter="afterEnter">
+            <div class="ball" v-show="show">
+                <div class="inner">
+                    <div class="cubeic-add"></div>
+                </div>
+            </div>
+        </transition>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "cartAnim",
+        data() {
+            return { show: false }
+        },
+        methods: {
+            start(el) { //启动动画接口，传递点击按钮元素
+                this.el = el;
+
+                //使.ball显示，激活动画钩子
+                this.show = true;
+            },
+            beforeEnter(el){
+                // 把小球移动到点击的dom园所所在的位置
+                const rect = this.el.getBoundingClientRect();
+                // 转换为用于绝对定位的坐标
+                const x = rect.left - window.innnerWidth / 2;
+                const y = -(window.innerHeight - rect.top - 10 - 20);
+
+            }
+        },
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
